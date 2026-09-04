1b0b0db13430dca2686833c51671ca6a8d0450a9
wodon326 <wodon326@naver.com>
2026-09-05 03:05:44 +0900

    포트폴리오 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
       {
         name: 'Robot',
         description:
-          '- [black]한국전자기술연구원 KETI[/black]\n  - LLM/VLM 기반 물류로봇 자율주행 시스템\n  - LiDAR-IMU 기반 SLAM 매핑 및 자율주행 시스템\n- [black]개인 프로젝트[/black]\n  - 강화학습 기반 로봇팔 핀-홀 정밀 삽입 시스템\n  - 강화학습 기반 AMR 물류로봇 자율주행 시스템',
+          '- [black]한국전자기술연구원 KETI[/black]\n  - LLM/VLM 기반 자연어 지시형 물류로봇 다층 자율주행 시스템 개발\n  - 국산 NPU·RGB-D AI 시각센서 기반 로봇 이동지능 실증\n- [black]개인 프로젝트[/black]\n  - 강화학습 기반 로봇팔 핀-홀 정밀 삽입 시스템\n  - 강화학습 기반 AMR 물류로봇 자율주행 시스템',
         link: '',
       },
       {
